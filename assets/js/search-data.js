eec3e44c307d8bf5360eb76cc288b21dba3a88e6
aden9460 <76890853+aden9460@users.noreply.github.com>
2025-11-29 08:29:37 +0000

    Deploying to gh-pages from @ aden9460/Zefang-Wang@778de38be1df52040a68e2cb029b72899b7a1da0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -104,25 +104,11 @@ ninja.data = [{
           window.open("https://github.com/aden9460", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
         id: 'social-rss',
         title: 'RSS Feed',
         section: 'Socials',
         handler: () => {
           window.open("/Zefang-Wang/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
         },
       },{
         id: 'social-x',
